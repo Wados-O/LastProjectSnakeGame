0c32b7f621f215aca793e800dd1aa93872f24ad1
Dmitri Mandaji <badmopss@gmail.com>
2023-04-18 17:50:33 +0200

    add the GameScore

--- src/GameScore.java
+++ src/GameScore.java
@@ -1,5 +1,23 @@
-//Добавьте счетчик очков
 
+import javax.swing.*;
 
-public class GameScore {
+public class GameScore extends JPanel {
+  private JLabel scoreLabel;
+  private int score;
+
+  public GameScore() {
+    score = 0;
+    scoreLabel = new JLabel("Score: " + score);
+    add(scoreLabel);
+  }
+
+  public void addScore() {
+    score++;
+    scoreLabel.setText("Score: " + score);
+  }
+
+  public void resetScore() {
+    score = 0;
+    scoreLabel.setText("Score: " + score);
+  }
 }
--- src/SnakeFood.java
+++ src/SnakeFood.java
@@ -43,5 +43,4 @@ public class SnakeFood {
       return y;
     }
   }
-
 }
