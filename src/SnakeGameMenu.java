7b16d06b1ea35f8de5b3d5e975a1120d2f3b14f8
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-04-20 00:18:31 +0200

    feat : Start and Exit button

--- src/SnakeGameMenu.java
+++ src/SnakeGameMenu.java
@@ -1,7 +1,26 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class SnakeGameMenu extends JPanel {
   public SnakeGameMenu(){
+
+    setLayout(new BorderLayout());
+    setBackground(Color.BLACK);
+
+    // Создание кнопок для главного меню
+    JButton startButton = new JButton("Start the GAME");
+    JButton exitButton = new JButton("Exit");
+
+    // Добавление кнопок на панель
+    JPanel buttonPanel =new JPanel();
+    buttonPanel.setLayout(new GridLayout(2, 1));
+    buttonPanel.add(startButton);
+    buttonPanel.add(exitButton);
+    add(buttonPanel, BorderLayout.CENTER);
+
 
 
   }
