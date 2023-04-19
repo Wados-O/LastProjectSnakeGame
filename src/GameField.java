17a71ac916d42b682d9753faefef6ff7fdebfec4
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-04-20 00:31:42 +0200

    add class GameField

---
+++ src/GameField.java
@@ -0,0 +1,12 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.jar.JarEntry;
+import javax.swing.JPanel;
+
+public class GameField extends JPanel implements ActionListener {
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+
+  }
+}
