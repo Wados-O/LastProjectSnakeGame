de88ed6fbef8e0412435fbdea34a495dd312ba3c
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-04-19 23:55:37 +0200

    create : SnakeGameMenu.java

---
+++ src/SnakeGameMenu.java
@@ -0,0 +1,23 @@
+import javax.swing.JPanel;
+
+public class SnakeGameMenu extends JPanel {
+  public SnakeGameMenu(){
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+
+
+  }
+}
