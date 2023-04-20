fea14b361188096b9e79c2a5e66257a7435e4023
Eugen Borodin <Azsalatay@gmail.com>
2023-04-20 15:35:30 +0200

    Add Class Sound.java

---
+++ src/Sound.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import javax.sound.sampled.*;
+
+public class Sound {
+  private Clip clip;
+
+  public void load(String filename) throws Exception {
+    AudioInputStream ais = AudioSystem.getAudioInputStream(
+        new File(filename));
+    clip = AudioSystem.getClip();
+    clip.open(ais);
+  }
+
+
+
+  public void play() {
+    clip.setFramePosition(0);
+    clip.start();
+  }
+}
