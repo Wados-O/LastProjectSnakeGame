a9c2fcb3fe7a26c8c40d7aec1f144622cbde14d0
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-04-23 19:23:10 +0200

    feat : "create test button  exit and start"

--- src/SnakeGameMenuTest.java
+++ src/SnakeGameMenuTest.java
@@ -1,7 +1,12 @@
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import java.awt.Component;
+import java.awt.Container;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -27,4 +32,53 @@ public class SnakeGameMenuTest {
     assertTrue(JFrame.getFrames().length > 0);
     thread.interrupt();
   }
+
+  @Test
+  public void testStartButton() {
+    SnakeGameMenu menu = new SnakeGameMenu();
+    Component[] components = menu.getComponents();
+    for (Component component : components) {
+      if (component instanceof JPanel) {
+        JPanel panel = (JPanel) component;
+        Component[] panelComponents = panel.getComponents();
+        for (Component panelComponent : panelComponents) {
+          if (panelComponent instanceof JButton) {
+            JButton button = (JButton) panelComponent;
+            if (button.getText().equals("START")) {
+              assertTrue(button.isEnabled());
+              assertTrue(button.isVisible());
+              return;
+            }
+          }
+        }
+      }
+    }
+    fail("Button not found.");
+  }
+  @Test
+  public void testExitButton(){
+    SnakeGameMenu menu = new SnakeGameMenu();
+    JButton exitButton = getMenuExitButton(menu);
+    assertNotNull(exitButton);
+    assertTrue(exitButton.isEnabled());
+    assertTrue(exitButton.isVisible());
+  }
+
+  private JButton getMenuExitButton(Container container) {
+    Component[] components = container.getComponents();
+    for (Component component : components) {
+      if (component instanceof JPanel) {
+        JPanel panel = (JPanel) component;
+        Component[] panelComponents = panel.getComponents();
+        for (Component panelComponent : panelComponents) {
+          if (panelComponent instanceof JButton && ((JButton) panelComponent).getText().equals("EXIT")) {
+            return (JButton) panelComponent;
+          }
+        }
+      }
+    }
+    return null;
+  }
+
 }
+
